1f5e546b34db57253cf262c8433c404b4c191515
Stephanie Siaw <stephaniesiaw@Stephanies-MacBook-Air.local>
2013-11-22 10:33:37 +0800

    Can only select box on certain date

--- javascript/jiggler.js
+++ javascript/jiggler.js
@@ -1,23 +1,23 @@
-$(document).ready(function () {
-  var animations = [ 'shake',
-             'hop',
-             'spin',
-             'hooray' ];
+// $(document).ready(function () {
+//   var animations = [ 'shake',
+//              'hop',
+//              'spin',
+//              'hooray' ];
 
-  function getRandomInt (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+//   function getRandomInt (min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+//   }
 
-  $('.box').on ('click', function () {
-    var meat = this;
-    var animation = animations[getRandomInt(0, 3)];
+//   $('.box').on ('click', function () {
+//     var meat = this;
+//     var animation = animations[getRandomInt(0, 3)];
 
-    $(meat).addClass(animation);
+//     $(meat).addClass(animation);
 
-    setTimeout(function () {
-      $(meat).removeClass(animation);
-    }, 2100);
-  });
+//     setTimeout(function () {
+//       $(meat).removeClass(animation);
+//     }, 2100);
+//   });
 
 
-});
+// });
